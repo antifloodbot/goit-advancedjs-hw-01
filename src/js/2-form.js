e86db4b814a4c91fcaba922be5c74c00846846c4
Kostian <kostia.talamaniuk@gmail.com>
2025-11-17 08:24:21 +0000

    initial

---
+++ src/js/2-form.js
@@ -0,0 +1,63 @@
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+const { email, message } = form.elements;
+
+loadFormData();
+
+form.addEventListener(
+  'input',
+  throttle(event => {
+    const { name, value } = event.target;
+
+    if (!(name in formData)) return;
+
+    formData[name] = value.trim();
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  }, 500)
+);
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  formData.email = email.value.trim();
+  formData.message = message.value.trim();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  form.reset();
+  localStorage.removeItem(STORAGE_KEY);
+
+  formData.email = '';
+  formData.message = '';
+});
+
+function loadFormData() {
+  const saved = localStorage.getItem(STORAGE_KEY);
+  if (!saved) return;
+
+  try {
+    const parsed = JSON.parse(saved);
+
+    formData.email = parsed.email?.trim() || '';
+    formData.message = parsed.message?.trim() || '';
+
+    email.value = formData.email;
+    message.value = formData.message;
+  } catch (error) {
+    console.error('Error parsing saved form data:', error);
+  }
+}
---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import { glob } from 'glob';
+import SortCss from 'postcss-sort-media-queries';
+
+export default defineConfig(({ command }) => ({
+  base: '/goit-advancedjs-hw-01/',
+  define: { [command === 'serve' ? 'global' : '_global']: {} },
+
+  root: 'src',
+
+  server: {
+    open: '/index.html',
+  },
+
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      input: glob.sync('./src/*.html'),
+    },
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+
+  css: {
+    postcss: {
+      plugins: [SortCss({ sort: 'mobile-first' })],
+    },
+  },
+}));
